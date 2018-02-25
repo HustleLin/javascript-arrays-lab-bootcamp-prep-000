8a638b427a8c7628411619778ed4bbd1b6e6adae
Jonathan Lin <jlin8293@gmail.com>
2018-02-25 18:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,54 +24,7 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 
-
-// var kittens = ["Milo", "Otis", "Garfield"]
-//
-//
-// function destructivelyAppendKitten(name) {
-//   kittens.push(name);
-//   console.log(kittens);
-// }
-//
-// function destructivelyPrependKitten(name) {
-//   kittens.unshift(name);
-//   return kittens;
-// }
-//
-// function destructivelyRemoveLastKitten(name) {
-//   kittens.pop(name);
-//   return kittens;
-// }
-//
-// function destructivelyRemoveFirstKitten(name) {
-// <<<<<<< HEAD
-//   kittens.shift(name);
-//   return kittens;
-// }
-// function appendKitten(name) {
-//   var newArray = [...kittens, name];
-//   return newArray;
-// }
-//
-// function prependKitten(name) {
-//   var newArray = [name, ...kittens];
-//   return newArray;
-// }
-//
-// function removeLastKitten(name) {
-//   var newArray = kittens.slice(0, kittens.length - 1);
-//   return newArray;
-// }
-//
-// function removeFirstKitten(name) {
-//   var newArray = kittens.slice(1, kittens.length);
-//   return newArray
-// =======
-//   kittens.shift;
-//   return kittens;
-// }
-// function appendKitten(name) {
-//   kittens = [...kittens, name]
-//   return kittens
-// >>>>>>> 956d671e09bf3922dee4f8d8bc6752892ff0f68a
-// }
+function removeLastKitten(name) {
+  kittens.slice(0, kittens.length - 1);
+  return kittens;
+}
