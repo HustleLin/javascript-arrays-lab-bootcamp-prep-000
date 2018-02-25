c381ac43461ebaaeccef737c92f40f60b347a9aa
Jonathan Lin <jlin8293@gmail.com>
2018-02-25 18:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,5 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-  kittens.slice(0, kittens.length - 1);
-  return kittens;
+  return kittens.slice(0, kittens.length - 1);
 }
